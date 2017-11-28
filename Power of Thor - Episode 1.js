ec93cd1a00b302f4127ce558e4b494d0fb532906
Richard Ellery <raellery@gmail.com>
2017-11-28 20:44:41 +0900

    Add finished thor solution

--- Power of Thor - Episode 1.js
+++ Power of Thor - Episode 1.js
@@ -8,8 +8,8 @@
 var inputs = readline().split(' ');
 var lightX = parseInt(inputs[0]); // the X position of the light of power
 var lightY = parseInt(inputs[1]); // the Y position of the light of power
-var initialTX = parseInt(inputs[2]); // Thor's starting X position
-var initialTY = parseInt(inputs[3]); // Thor's starting Y position
+var thorX = parseInt(inputs[2]); // Thor's starting X position
+var thorY = parseInt(inputs[3]); // Thor's starting Y position
 
 // game loop
 while (true) {
@@ -17,8 +17,35 @@ while (true) {
 
     // Write an action using print()
     // To debug: printErr('Debug messages...');
+    var directionY = "";
+    var directionX = "";
     var direction = "";
-
+    
+    if (lightY > thorY) {
+      directionY = "S";
+      thorY++;
+    }
+    else if (lightY < thorY) {
+      directionY = "N";
+      thorY--;
+    }
+    else {
+      directionY = "";
+    }
+  
+    if (lightX > thorX) {
+      directionX = "E";
+      thorX++;
+    }
+    else if (lightX < thorX) {
+      directionX = "W";
+      thorX--;
+    }
+    else {
+      directionX = "";
+    } 
+    direction = directionY + directionX;
+    
     // A single line providing the move to be made: N NE E SE S SW W or NW
     print(direction);
 }
